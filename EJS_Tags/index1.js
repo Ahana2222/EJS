8ea9a23eaec897f78db172b07fab872128f87600
Ahana Datta <22051831@kiit.ac.in>
2023-09-30 02:09:34 +0530

    first commit

---
+++ EJS_Tags/index1.js
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res) => {
+    const data = {
+        title: "EJS Tags",
+        seconds: new Date().getSeconds(),
+        items: ["apple","banana","cherry"],
+        htmlContent: "<strong>This is some strong text</strong>",
+    };
+    res.render("index1.ejs",data);
+});
+
+app.listen(port,(err) => {
+    if (err) throw err;
+    console.log(`Server is running on port ${port}`);
+});
